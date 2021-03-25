3f6fc44e9354201dc0fa8467c4d451185b5d91dc
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-03-25 11:56:36 +0700

    server side searching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, {Component} from 'react';
 import './App.css';
 
+const URL = 'https://jsonplaceholder.typicode.com/posts'
+
 class App extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             posts: [],
-            searchboxText: ''
+            searchboxText: '',
+            searchboxTextServerSide: '',
         }
 
         this.handleSearchingClientSide = this.handleSearchingClientSide.bind(this);
+        this.handleSearchingServerSide = this.handleSearchingServerSide.bind(this);
     }
 
     handleSearchingClientSide(text) {
         this.setState({searchboxText: text})
+    }
+
+    handleSearchingServerSide(text) {
+        this.setState({searchboxTextServerSide: text})
+
+        let endpoint = URL;
+        if (text) {
+            endpoint += '?userId=' + text
+        }
+
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(json => this.setState({'posts': json}))
+
     }
 
     componentDidMount() {
@@ -22,7 +41,7 @@ class App extends Component {
         // fetch('https://jsonplaceholder.typicode.com/posts')
         //     .then(res => this.setState({'posts': res.json()}))
 
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(URL)
             .then(res => res.json())
             .then(json => this.setState({'posts': json}))
 
@@ -31,23 +50,30 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <Table data={ this.state.posts }
+                <Table data={this.state.posts}
                        onSearchingClientSide={this.handleSearchingClientSide}
-                       searchboxText={ this.state.searchboxText }/>
+                       onSearchingServerSide={this.handleSearchingServerSide}
+                       searchboxText={this.state.searchboxText}
+                       searchboxTextServerSide={this.state.searchboxTextServerSide}/>
             </div>
         );
     }
 }
 
-class Table extends Component{
+class Table extends Component {
 
     constructor(props) {
         super(props);
         this.handleSearchClientSide = this.handleSearchClientSide.bind(this);
+        this.handleSearchServerSide = this.handleSearchServerSide.bind(this);
     }
 
     handleSearchClientSide(e) {
         this.props.onSearchingClientSide(e.target.value);
+    }
+
+    handleSearchServerSide(e) {
+        this.props.onSearchingServerSide(e.target.value);
     }
 
     render() {
@@ -63,13 +89,18 @@ class Table extends Component{
                             <input value={this.props.searchboxText}
                                    onChange={this.handleSearchClientSide}
                                    type="text"
-                                   name="search" placeholder="Client side searching..."/>
-                            <input type="text" placeholder="Server side searching..."/>
+                                   name="search" placeholder="Client side searching, filter by Title..."/>
+
+                            <input type="text"
+                                   value={this.props.searchboxTextServerSide}
+                                   onChange={this.handleSearchServerSide}
+                                   placeholder="Server side searching, filter by user ID..."/>
                         </form>
                     </th>
                 </tr>
                 <tr>
                     <th>Id</th>
+                    <th>User Id</th>
                     <th>Title</th>
                     <th>Body</th>
                 </tr>
@@ -79,12 +110,13 @@ class Table extends Component{
                     return (
                         <tr key={index}>
                             <td>{post.id}</td>
+                            <td>{post.userId}</td>
                             <td>{post.title}</td>
                             <td>{post.body}</td>
                         </tr>
                     )
                 }) : <tr>
-                    <td colSpan="3">{this.props.searchboxText ? 'Empty result' : 'Loading'}</td>
+                    <td colSpan="3">{this.props.searchboxText || this.props.searchboxTextServerSide ? 'Empty result' : 'Loading'}</td>
                 </tr>}
                 </tbody>
             </table>
