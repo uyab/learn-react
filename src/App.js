6253ced930454ddba26cb4346b147f21e40f7460
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-03-25 10:27:01 +0700

    menampilkan data dari API ke tabel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <Table data="test" />
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
+
+    componentDidMount() {
+
+        // It doesn't work
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(res => this.setState({'posts': res.json()}))
+
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(json => this.setState({'posts': json}))
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Table data={ this.state.posts }/>
+            </div>
+        );
+    }
 }
 
 function Table(props) {
     return (
         <table>
+            <caption>Jumlah posts: {props.data.length}</caption>
             <thead>
             <tr>
                 <th>Id</th>
@@ -20,11 +42,17 @@ function Table(props) {
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+            {(props.data.length > 0) ? props.data.map((post, index) => {
+                return (
+                    <tr key={index}>
+                        <td>{post.id}</td>
+                        <td>{post.title}</td>
+                        <td>{post.body}</td>
+                    </tr>
+                )
+            }) : <tr>
+                <td colSpan="3">Loading...</td>
+            </tr>}
             </tbody>
         </table>
     );
